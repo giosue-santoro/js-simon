c00c225b5dac615ca5d03da5ead5a38e740de3f0
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-01-15 16:49:28 +0100

    numeri pc mostrati all'utente

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 //Selettori
 var userNumber;
-var pcNumber;
 var userArray;
 var pcArray;
+var minRange = 1;
+var maxRange = 20;
+var pcLength = 5;
+
 
 //Funzione per creare i numeri casuali
 function randomN(min, max) {
@@ -21,3 +24,17 @@ function numberCheck(userNumber,userArray) {
         return false;
     }
 }
+
+//Genero e mostro numeri da ricordare all'utente
+pcArray = [];
+userArray = [];
+
+while (pcArray.length < pcLength) {
+    n = randomN(minRange, maxRange);
+    if (pcArray.indexOf(n) === -1) {
+        pcArray.push(n);
+    }
+}
+
+console.log('Questi sono i numeri generati dal pc: ' + pcArray);
+alert('Questi sono i numeri generati dal pc: ' + pcArray);
